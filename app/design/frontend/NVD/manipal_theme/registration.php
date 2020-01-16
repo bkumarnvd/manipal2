af2f8800dd7fc19d82594e03e34c59733d2962d4
karthik <bkumar@newvisiondigital.co>
2020-01-16 12:09:22 +0530

    New Child theme for manipal Hospitals

---
+++ app/design/frontend/NVD/manipal_theme/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Copyright © 2017 Magento. All rights reserved.
+* See COPYING.txt for license details.
+*/
+\Magento\Framework\Component\ComponentRegistrar::register(
+\Magento\Framework\Component\ComponentRegistrar::THEME,
+'frontend/nvd/manipal_theme',
+__DIR__
+);
